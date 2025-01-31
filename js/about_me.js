2fad81de756638239dfdd562d939999c0dc62492
AnantaPoudyal <130949112+AnantaPoudyal@users.noreply.github.com>
2025-01-31 12:27:14 +0545

    updated index.html

--- js/about_me.js
+++ js/about_me.js
@@ -1,7 +1,7 @@
 function about_me(){
-    var intro = document.getElementById("my_introduction");
+    var intro = document.getElementById("my_introduction").value;
     intro.innerHTML = "Hello, my name is <strong>John Doe</strong>. I am a <strong>web developer</strong> and I am currently learning <strong>JavaScript</strong>.";
-    
+
     console.log(intro);
 }
 about_me();
